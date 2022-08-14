a6102369113ac10592110dd14ca0af8e8d2c31f7
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-14 14:02:14 +0600

    changed project and repository name into 'basic_project'

--- basic_project/basic_project/urls.py
+++ basic_project/basic_project/urls.py
@@ -1,4 +1,4 @@
-"""basic_app URL Configuration
+"""basic_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
