34b7ba1fbe18d8f0bee939208b3bab51a80ed960
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-15 14:34:41 +0600

    shown custom 'string' into the admin panel

--- basic_project/basic_app/models.py
+++ basic_project/basic_app/models.py
@@ -6,6 +6,8 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
-    desc = models.CharField(max_length=2000)
+    desc = models.TextField()
     date = models.DateField() 
     
+    def __str__(self) -> str:
+        return f"Name: [ {self.name} ] -    Desc: [ {self.desc} ]"
