310c87d8377100ae6b13cf573a23f544f5895626
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-15 14:18:14 +0600

    fixed issue: 'desc' field's type changed to CharFiled()

--- basic_project/basic_app/models.py
+++ basic_project/basic_app/models.py
@@ -6,6 +6,6 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
-    desc = models.TimeField()
+    desc = models.CharField(max_length=2000)
     date = models.DateField() 
     
--- basic_project/basic_app/views.py
+++ basic_project/basic_app/views.py
@@ -1,7 +1,12 @@
 from multiprocessing import context
+from xmlrpc.client import DateTime
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from basic_app.models import Contact
 
 # Create your views here.
+
+
 def index(request):
     context = {
         "variable_1": "This is variable 1",
@@ -9,10 +14,24 @@ def index(request):
     }
     return render(request, "index.html", context)
     # return HttpResponse("This is HOME page")
-    
+
+
 def about(request):
     return render(request, "about.html")
+
+
 def services(request):
     return render(request, "services.html")
+
+
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone,
+                          desc=desc, date=datetime.today())
+        contact.save()
+
     return render(request, "contact.html")
